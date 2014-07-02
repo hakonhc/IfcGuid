a1a1034e4599384bbea034390eb5b894cd1f0ba4
Håkon Clausen <hhc@drofus.no>
2014-07-02 11:14:36 +0200

    Initial git version

---
+++ IfcGuid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IfcGuid
+{
+    /// <summary>
+    /// Test program
+    /// </summary>
+  public class Program
+  {
+    static void Main( string[] args )
+    {
+      Guid a = Guid.NewGuid();
+      Console.WriteLine(".NET GUID: " + a.ToString());
+      string ifc_guid = IfcGuid.ToIfcGuid( a );
+      Console.WriteLine("IFC GUID: " + ifc_guid);
+      Guid b = IfcGuid.FromIfcGuid( ifc_guid );
+      Console.WriteLine("Back to .NET GUID: " + b.ToString());
+      System.Diagnostics.Debug.Assert(a == b, "GUID does not match");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    using System.Diagnostics;
+
+    using IfcGuid;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var id = Guid.NewGuid();
+            var ifcGuid = IfcGuid.ToIfcGuid(id);
+            var back = IfcGuid.FromIfcGuid(ifcGuid);
+            Assert.AreEqual(back, id);
+            Debug.WriteLine(ifcGuid, id.ToString());
+        }
+
+        [TestMethod]
+        public void IfcGuidTestCases()
+        {
+            string ifcguid = "01psB8wRl0Y00000000005";
+            Assert.AreEqual(ifcguid, IfcGuid.ToIfcGuid(IfcGuid.FromIfcGuid(ifcguid)));
+        }
+
+        [TestMethod]
+        public void GuidTestCases()
+        {
+            string guid = "01cf62c8-e9bc-bf88-0000-000000000005";
+            var res = IfcGuid.FromIfcGuid(IfcGuid.ToIfcGuid(new Guid(guid)));
+            Assert.AreEqual(guid, res.ToString());
+        }
+
+        [TestMethod]
+        public void TestCompare()
+        {
+            string guid = "01cf62c8-e9bc-bf88-0000-000000000005";
+            string ifcguid = "01psB8wRo$Y00000000005";
+            var convertedGuid = IfcGuid.ToIfcGuid(new Guid(guid));
+            Assert.AreEqual(convertedGuid, ifcguid);
+        }
+    }
+}
